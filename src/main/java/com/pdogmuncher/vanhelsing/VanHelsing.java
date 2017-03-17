98b0b96f66364fa42cca78e3fc196a485d9e9c97
bengelman <brendan.engelman@gmail.com>
2017-03-17 17:00:22 -0600

    Fixed some things

--- src/main/java/com/pdogmuncher/vanhelsing/VanHelsing.java
+++ src/main/java/com/pdogmuncher/vanhelsing/VanHelsing.java
@@ -1,10 +1,13 @@
 package com.pdogmuncher.vanhelsing;
 
+
 import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = VanHelsing.MODID, version = VanHelsing.VERSION)
@@ -23,7 +26,15 @@ public class VanHelsing
     {
         
     }
+    @EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		VanHelsingEventHandler handler = new VanHelsingEventHandler();
+		MinecraftForge.EVENT_BUS.register(handler);
+    }
     public void initItems(){
-    	
+    	//Register an item here, using the proxy.initItem() method.
+    	//It will automatically set the item's unlocalized name as well as register
+    	//its texture. However, the lang file must be used to set the item's
+    	//in-game display name.
     }
 }
